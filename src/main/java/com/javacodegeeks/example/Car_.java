760a71925f69d548ed3b474507fd3b03be40394c
AisleiÁvila <aislei@outlook.com.br>
2021-03-26 14:12:20 -0300

    meu primeiro commit

---
+++ src/main/java/com/javacodegeeks/example/Car_.java
@@ -0,0 +1,12 @@
+package com.javacodegeeks.example;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2019-07-22T00:12:19.919-0300")
+@StaticMetamodel(Car.class)
+public class Car_ {
+	public static volatile SingularAttribute<Car, String> model;
+	public static volatile SingularAttribute<Car, String> price;
+}
